569ec7bfbe80c111e39a133b2f90b2e72053e32d
14sh <hannasilvaventura21@gmail.com>
2026-02-26 12:00:18 -0300

    Update desafio_1003.py

--- desafio_1003.py
+++ desafio_1003.py
@@ -14,4 +14,10 @@ de A e B.
 
 # Link do problema: https://judge.beecrowd.com/pt/problems/view/1003
 
-# Escreva sua solução abaixo
+# Escreva sua solução abaixo 
+A = int(input())
+B = int(input())
+
+SOMA = A + B
+
+print(f"SOMA = {SOMA}")
